7e8ae023c6bd23e4a2caa479338e2e97cc31c1c4
petrosyan4ik <54333564+petrosyan4ik@users.noreply.github.com>
2019-08-25 14:26:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-app.js
+var http = require('http');
+
+http.createServer(function (req, res){
+res.writeHead(200, {'content-type':'text/plain'});
+res.end('Hi guys!!!
+My name is Vlad Iam 31 years old I want to become a programmer! I love tea with cookies!)))');
+}) .listen(9001);
+console.log('Server is running on a prt over 9000!!!');
